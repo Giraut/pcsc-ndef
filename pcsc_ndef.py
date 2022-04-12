700d310d8749c108de1d3c60b5336325c914b51c
Giraut <>
2022-04-12 07:12:08 +0300

    Reverted to shared PC/SC card usage, as exclusive seems to lock up the application at random

--- pcsc_ndef.py
+++ pcsc_ndef.py
@@ -200,7 +200,7 @@ class pcsc_ndef():
       try:
         r, hcard, dwActiveProtocol = sc.SCardConnect(self.hcontext,
 						self.reader,
-						sc.SCARD_SHARE_EXCLUSIVE,
+						sc.SCARD_SHARE_SHARED,
 						sc.SCARD_PROTOCOL_T0 | \
 						sc.SCARD_PROTOCOL_T1)
 
